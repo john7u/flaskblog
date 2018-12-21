7acfa201d962d716c3b21e107cf7649a0543251d
johnauerbach <since1919@126.com>
2018-12-21 17:32:18 +0800

    添加pagedown

--- app/__init__.py
+++ app/__init__.py
@@ -8,12 +8,14 @@ from flask_sqlalchemy import SQLAlchemy
 # import pymysql
 from config import config
 from flask_login import LoginManager
+from flask_pagedown import PageDown
 
 
 bootstrap = Bootstrap()
 mail = Mail()
 moment = Moment()
 db = SQLAlchemy()
+pagedown = PageDown()
 login_manager = LoginManager()
 login_manager.session_protection = 'strong'
 login_manager.login_view = 'auth.login'
@@ -30,6 +32,7 @@ def create_app(config_name):
     mail.init_app(app)
     moment.init_app(app)
     db.init_app(app)
+    pagedown.init_app(app)
     login_manager.init_app(app)
 
     from .main import main as main_blueprint
