c36735384d84d62739789213dbc66a054289939f
johnauerbach <since1919@126.com>
2019-01-12 20:20:39 +0800

    添加服务端markdown转换html

--- app/models.py
+++ app/models.py
@@ -8,6 +8,8 @@ from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
 from flask import current_app, request
 from datetime import datetime
 import hashlib
+from markdown import markdown
+import bleach
 
 
 class Role(db.Model):
@@ -217,6 +219,7 @@ class Post(db.Model):
     body = db.Column(db.Text)
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
     author_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    body_html = db.Column(db.Text)
 
     # 生成虚拟文章
     @staticmethod
@@ -234,6 +237,19 @@ class Post(db.Model):
                      author=u)
             db.session.add(p)
             db.session.commit()
+
+    # body的markdown转换成body_html的html并去除一些标签
+    @staticmethod
+    def on_changed_body(target, value, oldvalue, initiator):
+        allowed_tags = ['a', 'abbr', 'acronym', 'b', 'blockquote', 'code',
+                        'em', 'i', 'li', 'ol', 'pre', 'strong', 'ul',
+                        'h1', 'h2', 'h3', 'p']
+        target.body_html = bleach.linkify(bleach.clean(markdown(value, output_format='html'),
+                                                       tags=allowed_tags, strip=True))
+
+
+# 开启sqlalchemy监听，每当body有更新，则调用on_changed_body
+db.event.listen(Post.body, 'set', Post.on_changed_body)
 
 
 # flask_login要求实现一个回调函数，接收Unicode字符串标书的用户标识符，
